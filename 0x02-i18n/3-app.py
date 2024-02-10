69b1571acbe262e7d827f3c5bf2f4bcd4cc5d199
Domodan <ongomdaniel9@gmail.com>
2024-02-10 13:41:12 +0000

    created a babel.cfg file containing configuration files

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -36,7 +36,7 @@ def get_index() -> str:
     """
         Render the Home/Index template page for Babel usage.
     """
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
